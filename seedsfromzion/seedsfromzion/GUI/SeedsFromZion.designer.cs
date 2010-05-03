f7c0fc9a355d3fbbc360bf7503517ee8cbe1f328
svalley.t2 <svalley.t2@c86af03a-c996-7ad9-e30c-4ee0a768d6fd>
2010-05-03 18:16:39 +0000

    added the handler to changing tab

--- seedsfromzion/seedsfromzion/GUI/SeedsFromZion.designer.cs
+++ seedsfromzion/seedsfromzion/GUI/SeedsFromZion.designer.cs
@@ -125,6 +125,7 @@
             this.systemControl.TabGroupHeight = 14;
             this.systemControl.TabIndex = 0;
             this.systemControl.Text = "systemControl";
+            this.systemControl.SelectedRibbonTabChanged += new System.EventHandler(this.systemControl_SelectedRibbonTabChanged);
             // 
             // inventoryPanel
             // 
@@ -134,10 +135,10 @@
             this.inventoryPanel.Controls.Add(this.generalInventoryBar);
             this.inventoryPanel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
             this.inventoryPanel.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.inventoryPanel.Location = new System.Drawing.Point(0, 56);
+            this.inventoryPanel.Location = new System.Drawing.Point(0, 61);
             this.inventoryPanel.Name = "inventoryPanel";
             this.inventoryPanel.Padding = new System.Windows.Forms.Padding(3, 0, 3, 3);
-            this.inventoryPanel.Size = new System.Drawing.Size(1001, 108);
+            this.inventoryPanel.Size = new System.Drawing.Size(1001, 103);
             this.inventoryPanel.TabIndex = 1;
             // 
             // ordersInventoryBar
@@ -152,7 +153,7 @@
             this.ordersInventoryBar.Location = new System.Drawing.Point(376, 0);
             this.ordersInventoryBar.Name = "ordersInventoryBar";
             this.ordersInventoryBar.ResizeOrderIndex = 1;
-            this.ordersInventoryBar.Size = new System.Drawing.Size(195, 105);
+            this.ordersInventoryBar.Size = new System.Drawing.Size(195, 100);
             this.ordersInventoryBar.Style = DevComponents.DotNetBar.eDotNetBarStyle.Office2007;
             this.ordersInventoryBar.TabIndex = 6;
             this.ordersInventoryBar.Text = "מנהל בהזמנות";
@@ -203,7 +204,7 @@
             this.operationsInventoryBar.Location = new System.Drawing.Point(181, 0);
             this.operationsInventoryBar.Name = "operationsInventoryBar";
             this.operationsInventoryBar.ResizeOrderIndex = 1;
-            this.operationsInventoryBar.Size = new System.Drawing.Size(195, 105);
+            this.operationsInventoryBar.Size = new System.Drawing.Size(195, 100);
             this.operationsInventoryBar.Style = DevComponents.DotNetBar.eDotNetBarStyle.Office2007;
             this.operationsInventoryBar.TabIndex = 5;
             this.operationsInventoryBar.Text = "פעלות";
@@ -254,7 +255,7 @@
             this.generalInventoryBar.Location = new System.Drawing.Point(3, 0);
             this.generalInventoryBar.Name = "generalInventoryBar";
             this.generalInventoryBar.ResizeOrderIndex = 1;
-            this.generalInventoryBar.Size = new System.Drawing.Size(178, 105);
+            this.generalInventoryBar.Size = new System.Drawing.Size(178, 100);
             this.generalInventoryBar.Style = DevComponents.DotNetBar.eDotNetBarStyle.Office2007;
             this.generalInventoryBar.TabIndex = 4;
             this.generalInventoryBar.Text = "כללי";
@@ -282,6 +283,7 @@
             this.addPlantButton.Tooltip = "הוספת צמח חדש...";
             this.addPlantButton.MouseLeave += new System.EventHandler(this.dotNetBarManager1_MouseLeave);
             this.addPlantButton.MouseEnter += new System.EventHandler(this.dotNetBarManager1_MouseEnter);
+            this.addPlantButton.Click += new System.EventHandler(this.addPlantButton_Click);
             // 
             // removePlantButton
             // 
@@ -843,9 +845,9 @@
             this.statusBar.GrabHandleStyle = DevComponents.DotNetBar.eGrabHandleStyle.ResizeHandle;
             this.statusBar.Items.AddRange(new DevComponents.DotNetBar.BaseItem[] {
             this.statusLabel});
-            this.statusBar.Location = new System.Drawing.Point(4, 584);
+            this.statusBar.Location = new System.Drawing.Point(4, 580);
             this.statusBar.Name = "statusBar";
-            this.statusBar.Size = new System.Drawing.Size(1001, 19);
+            this.statusBar.Size = new System.Drawing.Size(1001, 23);
             this.statusBar.Stretch = true;
             this.statusBar.Style = DevComponents.DotNetBar.eDotNetBarStyle.Office2007;
             this.statusBar.TabIndex = 1;
