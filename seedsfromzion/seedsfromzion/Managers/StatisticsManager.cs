bf197bd4431dd75eb5ef5cabd2933de692318986
svalley.t2 <svalley.t2@c86af03a-c996-7ad9-e30c-4ee0a768d6fd>
2010-05-06 15:56:51 +0000

    The statistics manager

--- seedsfromzion/seedsfromzion/Managers/StatisticsManager.cs
+++ seedsfromzion/seedsfromzion/Managers/StatisticsManager.cs
@@ -2,10 +2,32 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Data;
+using MySql.Data.MySqlClient;
+
+using seedsfromzion.DataAccess;
+
 
 namespace seedsfromzion.Managers
 {
     class StatisticsManager
     {
+        #region private methods
+        #endregion
+
+        #region public methods
+
+        public static DataTable getSalesGraphValues()
+        {
+            Int32 plantId = 222;
+            MySqlCommand command = 
+                DataAccessUtils.commandBuilder("SELECT O.orderDate AS orderDate,SUM(OFS.units) AS units FROM seedsdb.orders O ,seedsdb.ordersfromstorage OFS WHERE O.orderId = OFS.orderId AND OFS.plantId = @PLANT_ID GROUP BY YEAR(orderDate),MONTH(orderDate)",
+                                                "@PLANT_ID", plantId.ToString());
+            DataTable result = DatabaseAccess.getResultSetFromDb(command);
+            return result;
+        }
+        
+        #endregion
+
     }
 }
